e4c8254afc17e580829266c4c9411a25c1a08ef4
cherish3503 <90964197+cherish3503@users.noreply.github.com>
2022-03-13 21:43:00 +0900

    Create makeAP.py
    Wrong Answer

---
+++ round#764_B/makeAP.py
@@ -0,0 +1,12 @@
+import sys
+
+for _ in range(int(input())):
+  a,b,c = map(int, sys.stdin.readline().split())
+  if (2*b-a)%c == 0 and 2*b-a != 0: # c*k
+      print("YES")
+  elif (a+c)%(2*b) == 0: # b*k
+    print("YES")
+  elif (2*b-c)%a == 0 and 2*b-c != 0: # a*k
+    print("YES")
+  else:
+    print("NO")
